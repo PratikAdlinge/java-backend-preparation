eed015f1f45a61834c5964cb75cb168eaecf5df3
Pratik Adlinge <pratikadlinge2121@gmail.com>
2026-07-14 22:49:15 +0530

    another logic applied

--- DSA/Arrays/A01_LargestElement.java
+++ DSA/Arrays/A01_LargestElement.java
@@ -4,29 +4,27 @@ import java.util.Scanner;
 
 public class A01_LargestElement {
     public static void main(String[] args) {
-        
     
     Scanner sc=new Scanner(System.in);
-    System.out.println("enter size of an array:");
-    int size=sc.nextInt();
-    int arr[] =new int[size];
-    System.out.println("enter elements of an array :");
-
+    int size;
+    System.out.println("Enter the the size of an array");
+    size=sc.nextInt();
+    int arr[]=new int[size];
+    System.out.println("enter the elements of array:");
     for(int i=0;i<arr.length;i++) {
+
         arr[i]=sc.nextInt();
-
-    }
-    int largest=arr[0];
-    int secondLargest=arr[0];
-    for(int i=0;i<arr.length;i++) {
         
-      if(arr[i]>largest){
-        secondLargest=largest;
-        largest=arr[i];
-      }
     }
-    System.out.println(largest);
-    System.out.println(secondLargest);
+    int largest_Element=arr[0];
+    for(int i=0;i<arr.length;i++) {
 
-    } 
+        if(largest_Element<arr[i]){
+            largest_Element=arr[i];
+        }
+       // System.out.println("Element in Arrays:"+arr[i]);
+    }
+    System.out.println("Largest Element is:"+largest_Element);
+        
+    }
 }
