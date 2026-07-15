52ee51d66c3e41c4066cce9c0e6bf958abeb45f7
Pratik Adlinge <pratikadlinge2121@gmail.com>
2026-07-15 21:38:08 +0530

    smallest element

---
+++ DSA/Arrays/A02_Smallest_Element.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class A02_Smallest_Element {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of an Array:");
+        int size=sc.nextInt();
+        int arr1[]=new int[size];
+        System.out.println("Enter the elements in Array:");
+        
+        for(int i=0;i<arr1.length;i++){
+            arr1[i]=sc.nextInt();
+        }
+        int smallestElement=arr1[0];
+        for(int i=0;i<arr1.length;i++) {
+            if(smallestElement>arr1[i]){
+                smallestElement=arr1[i];
+            }
+        }
+        System.out.println("Smallest Element in Array is:"+smallestElement);
+    }
+}
