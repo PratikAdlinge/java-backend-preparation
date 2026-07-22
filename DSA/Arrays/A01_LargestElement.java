3634b2f0e6a5fc20e1eaf210db972bfeec235844
Pratik Adlinge <pratikadlinge2121@gmail.com>
2026-07-22 20:45:26 +0530

    Small changes in logic

--- DSA/Arrays/A01_LargestElement.java
+++ DSA/Arrays/A01_LargestElement.java
@@ -3,7 +3,7 @@ package DSA.Arrays;
 import java.util.Scanner;
 
 public class A01_LargestElement {
-    public static void main(String[] args) {
+    public static void main(String[] args) {                
     
     Scanner sc=new Scanner(System.in);
     int size;
