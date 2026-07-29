5e52320aa99c2dc36763addf1d7f6cb0bca9f0e6
PratikAdlinge <pratikadlinge2121@gmail.com>
2026-07-29 18:40:55 +0530

    add new logic

--- DSA/Arrays/A02_Smallest_Element.java
+++ DSA/Arrays/A02_Smallest_Element.java
@@ -1,22 +1,24 @@
+package DSA.Arrays;
 import java.util.Scanner;
+
 
 public class A02_Smallest_Element {
     public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the size of an Array:");
-        int size=sc.nextInt();
-        int arr1[]=new int[size];
-        System.out.println("Enter the elements in Array:");
-        
-        for(int i=0;i<arr1.length;i++){
-            arr1[i]=sc.nextInt();
+    Scanner sc=new Scanner(System.in);
+    int size;
+    System.out.println("Enter the size of an array:");  
+    size=sc.nextInt();
+    int arr1[]=new int[size];
+    System.out.println("Enter the elements of an array:");
+    for(int i=0;i<arr1.length;i++) {
+        arr1[i]=sc.nextInt();
+    }
+    int smallestElement=arr1[0];
+    for(int i=1;i<arr1.length;i++) {
+        if(smallestElement>arr1[i]){
+            smallestElement=arr1[i];
         }
-        int smallestElement=arr1[0];
-        for(int i=1;i<arr1.length;i++) {
-            if(smallestElement>arr1[i]){
-                smallestElement=arr1[i];
-            }
-        }
-        System.out.println("Smallest Element in Array is:"+smallestElement);
+    }
+    System.out.println("Smallest element in an array is:"+smallestElement);         
     }
 }
