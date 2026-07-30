58dc94d12692636444cce34c8300ada47e565292
PratikAdlinge <pratikadlinge2121@gmail.com>
2026-07-30 19:55:01 +0530

    changes some logic

--- OOPS/P01_Student_Class.java
+++ OOPS/P01_Student_Class.java
@@ -4,23 +4,18 @@ public class P01_Student_Class {
     String name;
     int rollNo;
     String address;
-    int age;
-
-    public void displayStudentDetails(){
-        System.out.println("Student Name "+name);
-        System.out.println("Student RollNo"+rollNo);
-        System.out.println("Student Address "+address);
-        System.out.println("Student Age "+age);
-     
+    public void display(){
+        System.out.println("Student Name is:"+name);
+        System.out.println("Student Roll no is:"+rollNo);
+        System.out.println("Student Address is:"+address);
     }
-    public static void main(String [] args){
-     
-    P01_Student_Class s1=new P01_Student_Class();
-    s1.name="Pratik";
-    s1.rollNo=101;
-    s1.address="Pune";  
-    s1.age=22;
-    s1.displayStudentDetails();
+    public static void main(String[] args){
+        P01_Student_Class p1=new P01_Student_Class();
+        p1.name="pratik";
+        p1.rollNo=101;
+        p1.address="Pune";
+        p1.display();
+    }
     }
     
-}
+
