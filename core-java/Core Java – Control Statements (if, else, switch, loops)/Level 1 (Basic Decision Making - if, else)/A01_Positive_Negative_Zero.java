5cf7bec3e0eeab81c6e63d43d9b49c8da60defe8
PratikAdlinge <pratikadlinge2121@gmail.com>
2026-07-31 14:43:00 +0530

    a Java program to determine whether a given integer is positive, negative, or zero using the if-else control statement.

---
+++ core-java/Core Java – Control Statements (if, else, switch, loops)/Level 1 (Basic Decision Making - if, else)/A01_Positive_Negative_Zero.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class A01_Positive_Negative_Zero {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a no ");
+        int no=sc.nextInt();
+        if (no==0) {
+            System.out.println("no is zero");
+        
+        }
+        else if(no>0){
+            System.out.println("no is positive");
+        }
+        else {
+            System.out.println("no is negative");
+        }
+    }
+}
