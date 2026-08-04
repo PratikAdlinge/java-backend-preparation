e9ca60d1714ca033053df9614002713a32a19b34
PratikAdlinge <pratikadlinge2121@gmail.com>
2026-08-04 20:12:19 +0530

    Left rotate array by one position

---
+++ DSA/Arrays/A11_LeftRotateByOne.java
@@ -0,0 +1,35 @@
+package DSA.Arrays;
+
+import java.util.Scanner;
+
+public class A11_LeftRotateByOne {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter size of an array");
+        int size=sc.nextInt();
+        if(size<0) {
+            System.out.println("Array must be greater than zero");
+            return;
+        }
+        int numbers[]=new int[size];
+        System.out.println("enter the elements of an array:");
+        for (int i=0;i<numbers.length;i++) {
+            numbers[i]=sc.nextInt();
+        }
+         int j=0; 
+        int first=numbers[0];
+        for (int i=1;i<numbers.length;i++) {
+            
+            numbers[j]=numbers[i];
+            j++;
+
+
+        }
+        numbers[numbers.length-1]=first;
+        
+        for (int i=0;i<numbers.length;i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+    
+}
