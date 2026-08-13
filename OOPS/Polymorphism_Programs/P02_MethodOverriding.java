93fddac64118aa5eb087030eb4ff833c56a9b4be
PratikAdlinge <pratikadlinge2121@gmail.com>
2026-08-13 17:07:02 +0530

    Added P02 method overriding example with runtime polymorphism and super keyword

---
+++ OOPS/Polymorphism_Programs/P02_MethodOverriding.java
@@ -0,0 +1,30 @@
+package OOPS.Polymorphism_Programs;
+class Animal {
+    void sound(){
+        System.out.println("Animal makes sound");
+    }
+
+}
+class Dog extends Animal{
+    @Override
+    void sound(){
+        super.sound();
+        System.out.println("Dog Barks:");
+    }
+}
+class Cat extends Animal{
+         @Override    
+         void sound(){
+            super.sound();
+            System.out.println("Cat Meows");
+        }
+
+}
+public class P02_MethodOverriding {
+    public static void main(String[] args) {
+        Animal d=new Dog();
+        Animal c=new Cat();
+        d.sound();
+        c.sound();
+    }
+}
