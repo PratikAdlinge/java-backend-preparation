287c16ebfbafc312059bf2036c91b9f3051df242
PratikAdlinge <pratikadlinge2121@gmail.com>
2026-08-13 18:37:41 +0530

    Added P01 encapsulation example with private variables, getters, setters, and data validation

---
+++ OOPS/Encapsulation_Programs/P01_Encapsulation.java
@@ -0,0 +1,48 @@
+package OOPS.Encapsulation_Programs;
+
+import java.util.Scanner;
+
+class BankAccount {
+    private int acc_No;private String acc_HolderName; private double balance;
+    public void setAccountNumber(int x){
+        this.acc_No=x;
+    }
+    public void setAccountHolderName(String x) {
+        this.acc_HolderName=x;
+    }
+    public void setAccountBalance(double x){
+        
+        if (x>=0) {
+            this.balance=x;
+            System.out.println("set Balance successfully");
+        } else {
+            System.out.println("invalid Balance:");
+        }
+        
+    }
+    public int getAccountNumber(){
+        return acc_No;
+    }
+    public String getAccountHolderName(){
+        return acc_HolderName;
+    }
+    public double getAccountBalance(){
+        
+        return balance;
+    }
+    
+}
+public class P01_Encapsulation {
+    public static void main(String[] args) {
+        BankAccount b1=new BankAccount();
+        b1.setAccountNumber(1012121393);
+        //System.out.println(acc_No);
+        b1.setAccountHolderName("Pratik Adlinge");
+        b1.setAccountBalance(-72345.89);     
+        System.out.println("Account No:"+b1.getAccountNumber());
+        System.out.println("Account holder Name:"+b1.getAccountHolderName());
+        System.out.println("Account Balance :"+b1.getAccountBalance());
+        
+
+    }
+}
