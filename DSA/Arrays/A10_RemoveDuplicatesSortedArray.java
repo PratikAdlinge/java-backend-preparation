ad2a831f7bc77878241fa2fec8560a439be504e8
PratikAdlinge <pratikadlinge2121@gmail.com>
2026-08-14 18:11:28 +0530

    remove undefine variables

--- DSA/Arrays/A10_RemoveDuplicatesSortedArray.java
+++ DSA/Arrays/A10_RemoveDuplicatesSortedArray.java
@@ -3,7 +3,7 @@ package DSA.Arrays;
 public class A10_RemoveDuplicatesSortedArray {
     public static void main(String[] args) {
         int arr1[]= {1,1,2,2,3,3,4,4,5};
-        int uniqueElementCounter=0;
+        //int uniqueElementCounter=0;
         int i=0;
         for (int j=1;j<arr1.length;j++) {
            
