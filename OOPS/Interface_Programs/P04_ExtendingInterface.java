272c11eac965de9cf48292640516b6f102e5ffd9
PratikAdlinge <pratikadlinge2121@gmail.com>
2026-08-15 15:59:59 +0530

    Added P04 extending interface example with interface inheritance and implementation

---
+++ OOPS/Interface_Programs/P04_ExtendingInterface.java
@@ -0,0 +1,23 @@
+package OOPS.Interface_Programs;
+
+interface Device {
+    void poweron();
+}
+interface Smartdevice extends Device {
+    void connectInternet();
+}
+class Smartphone implements Smartdevice {
+    public void poweron() {
+        System.out.println("Device is power on");
+    }
+    public void connectInternet(){
+        System.out.println("Smartphone connected to Internet");
+    }
+}
+public class P04_ExtendingInterface {
+    public static void main(String[] args) {
+        Smartdevice s1=new Smartphone();
+        s1.poweron();
+        s1.connectInternet();
+    }
+}
