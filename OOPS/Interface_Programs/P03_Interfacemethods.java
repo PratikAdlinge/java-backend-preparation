07865b0eb3b43460835f7a6cf62eb002c5a1587e
PratikAdlinge <pratikadlinge2121@gmail.com>
2026-08-15 15:24:05 +0530

    method and abstract class

---
+++ OOPS/Interface_Programs/P03_Interfacemethods.java
@@ -0,0 +1,25 @@
+package OOPS.Interface_Programs;
+
+interface Client {
+    void webdesign();
+    void webdevelop();
+
+}
+
+abstract class Sanket implements Client {
+    public void webdesign(){
+        System.out.println("Green , top menu ,navbar");
+    }
+}
+class Nishant extends Sanket {
+    public void webdevelop() {
+        System.out.println("HTML,CSS ,JAVASCRIPT");
+    }
+}
+public class P03_Interfacemethods {
+    public static void main(String[] args) {
+        Nishant n1=new Nishant();
+        n1.webdesign();
+        n1.webdevelop();
+    }
+}
