f37a152da9bb24f6778e0f0dc70f610c27056fec
PratikAdlinge <pratikadlinge2121@gmail.com>
2026-08-15 13:37:13 +0530

    Added P02 abstract method example with method overriding and runtime polymorphism

---
+++ OOPS/Abstraction_Programs/P02_AbstractMethod.java
@@ -0,0 +1,26 @@
+package OOPS.Abstraction_Programs;
+ abstract class Shape {
+    public abstract void calArea();
+ }
+ class Circle extends Shape {
+    double r=10.34;
+    public void calArea() {
+        double area=3.14*r*r;
+        System.out.println("area of circle:"+area);
+    }
+ }
+ class Rectangle extends Shape {
+    double l=24,b=12;
+    public void calArea(){
+        double area=l*b;
+        System.out.println("Area of rectangle:"+area);
+    }
+ }
+public class P02_AbstractMethod {
+    public static void main(String[] args) {
+        Shape s1=new Circle();
+        Shape s2=new Rectangle();
+        s1.calArea();
+        s2.calArea();
+    }
+}
