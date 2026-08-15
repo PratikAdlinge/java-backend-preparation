1cb38140df85a7facac1a1f89033a4a239ff4bf3
PratikAdlinge <pratikadlinge2121@gmail.com>
2026-08-15 14:24:11 +0530

    added simple interface program

---
+++ OOPS/Interface_Programs/P01_Interface.java
@@ -0,0 +1,34 @@
+package OOPS.Interface_Programs;
+
+import java.util.Scanner;
+
+interface Client {
+    public abstract void input();
+    public abstract void output();
+}
+class Developer implements Client {
+    Scanner sc=new Scanner(System.in);
+    String name;double sal;
+    @Override
+    public void input() {
+        System.out.println("Enter Employee Name:");
+        name=sc.next();
+        System.out.println("Enter the salary of Employee");
+        sal=sc.nextDouble();
+    }
+    @Override
+    public void output() {
+        System.out.println("Employee Details:");
+        System.out.println("Employee Name:"+name);
+        System.out.println("Employee Salary :"+sal);
+    }
+}
+public class P01_Interface {
+    public static void main(String[] args) {
+        Client c1=new Developer();
+        c1.input();
+        c1.output();
+
+    }
+    
+}
