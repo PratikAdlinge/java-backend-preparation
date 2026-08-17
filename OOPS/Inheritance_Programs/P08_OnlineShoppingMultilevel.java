8fc4cf6114b4aa8640df7cdd55fdddb4e8088f70
PratikAdlinge <pratikadlinge2121@gmail.com>
2026-08-17 22:52:04 +0530

    Add mew one

---
+++ OOPS/Inheritance_Programs/P08_OnlineShoppingMultilevel.java
@@ -0,0 +1,24 @@
+package OOPS.Inheritance_Programs;
+class Product{
+    String productName;double price;
+    Product (String productName,double price) {
+        this.productName=productName;
+        this.price=price;
+   }
+   void displayProduct() {
+    System.out.println("Product Name Is:"+productName);
+    System.out.println("Price of Product:"+price);
+   }
+}
+class ElectronicProduct extends Product{
+    String brand;
+    int warranty;
+    ElectronicProduct (String productName,double price,String brand,int warranty) {
+    this.brand=brand;
+    
+    }
+    
+}
+public class P08_OnlineShoppingMultilevel {
+    
+}
