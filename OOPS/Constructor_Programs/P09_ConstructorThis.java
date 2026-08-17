0c7e32f93cacc350def2f065dfac3944e47428d5
PratikAdlinge <pratikadlinge2121@gmail.com>
2026-08-17 18:10:57 +0530

    Added P09 constructor chaining using this() and constructor overloading

---
+++ OOPS/Constructor_Programs/P09_ConstructorThis.java
@@ -0,0 +1,35 @@
+package OOPS.Constructor_Programs;
+
+import java.util.Scanner;
+
+class Student121 {
+    Scanner sc=new Scanner(System.in);
+    Student121(){
+        String name;
+        System.out.println("Enter Student name:");
+        name=sc.next();
+        System.out.println("Name of Student :"+name);
+    }
+    Student121(int x){
+        this("java",112009.489);
+
+        int rollNo=x;
+        System.out.println("Roll No of student is :"+rollNo);
+    }
+    Student121(String course,double fees) {
+        String coursee;double fees1;
+
+         coursee=course;
+         fees1=fees;
+         System.out.println("Course of student "+coursee+"\n Fees of student : "+fees1);
+
+
+    }
+}
+public class P09_ConstructorThis {
+    public static void main(String[] args) {
+        Student121 S1=new Student121();
+        Student121 S2=new Student121(101);
+
+    }
+}
