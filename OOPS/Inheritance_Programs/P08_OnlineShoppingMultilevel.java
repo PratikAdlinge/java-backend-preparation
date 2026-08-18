26dad9846c4466bd491577fb710ef6e84647d612
PratikAdlinge <pratikadlinge2121@gmail.com>
2026-08-18 11:41:56 +0530

    Added P08 real-world multilevel inheritance example using Product, ElectronicProduct, and Mobile

--- OOPS/Inheritance_Programs/P08_OnlineShoppingMultilevel.java
+++ OOPS/Inheritance_Programs/P08_OnlineShoppingMultilevel.java
@@ -1,7 +1,7 @@
 package OOPS.Inheritance_Programs;
 class Product{
     String productName;double price;
-    Product (String productName,double price) {
+    public Product (String productName,double price) {
         this.productName=productName;
         this.price=price;
    }
@@ -13,14 +13,40 @@ class Product{
 class ElectronicProduct extends Product{
     String brand;
     int warranty;
-    ElectronicProduct (String productName,double price,String brand,int warranty) {
-    this.brand=brand;
-    this.warranty=warranty;
+    public ElectronicProduct (String productName,double price,String brand,int warranty) {
+        super(productName, price);
+        this.brand=brand;
+        this.warranty=warranty;
     }
-    
+    void electonicProductDisplay () {
+        System.out.println("Brand of Product is:"+brand);
+        System.out.println("Warranty of a Product is:"+warranty);
+    }
+
+}
+class Mobile extends ElectronicProduct {
+    String os;int storage;
+    Mobile (String productName,double price,String brand,int warranty,String os,int storage) {
+        super(productName, price, brand, warranty);
+        this.os=os;
+        this.storage=storage;
+    }
+    void displayMobileDetails() {
+        System.out.println("Product Name:"+productName);
+        System.out.println("Price of Product:"+price);
+        System.out.println("Brand of Product:"+brand);
+        System.out.println("Warranty of Product"+warranty);
+        System.out.println("Operating System of Product :"+os);
+        System.out.println("Storage of Product:"+storage);
+    }
 }
 public class P08_OnlineShoppingMultilevel {
     public static void main(String[] args) {
-        
+       Mobile mobile=new Mobile("Moto Edge 50 Fusion", 25000,"MOTOROLA", 1,"Android",256) ;
+       mobile.displayMobileDetails();
+       System.out.println();
+       mobile.displayProduct();
+       System.out.println();
+       mobile.electonicProductDisplay();
     }
 }
