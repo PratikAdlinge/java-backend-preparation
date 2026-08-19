16f5bed6383822a328cb12083dda7ee6cf0c9e23
PratikAdlinge <pratikadlinge2121@gmail.com>
2026-08-19 22:55:50 +0530

    solve issue

---
+++ OOPS/Polymorphism_Programs/P03_MethodOverloading.java
@@ -0,0 +1,10 @@
+package OOPS.Polymorphism_Programs;
+class BillCalculator {
+    int price;
+   void  calCulateBill(double price) {
+        this.price=price;
+    }
+}
+public class P03_MethodOverloading {
+    
+}
