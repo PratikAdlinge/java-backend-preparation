5778b81224b8afa8be70c20ade1a56cf0f1ac967
PratikAdlinge <pratikadlinge2121@gmail.com>
2026-08-19 22:47:54 +0530

    correct the naming convention

--- core-java/Core Java – Control Statements (if, else, switch, loops)/Level 1 (Basic Decision Making - if, else)/A09_Divisible_By_5_And_11.java
+++ core-java/Core Java – Control Statements (if, else, switch, loops)/Level 1 (Basic Decision Making - if, else)/A09_Divisible_By_5_And_11.java
@@ -4,16 +4,16 @@ public class A09_Divisible_By_5_And_11 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("enter a no ");
-        int no=sc.nextInt();
-        if (no%5==0 && no%11==0) {
+        int number=sc.nextInt();
+        if (number%5==0 && number%11==0) {
             System.out.println("number is divisible by both 5 and 11");
             
         }
-        else if(no%5==0) {
+        else if(number%5==0) {
             System.out.println("number is divisible by 5 only");
 
         }
-        else if(no%11==0) {
+        else if(number%11==0) {
             System.out.println("number is divisible by 11 only ");
          }
          else {
