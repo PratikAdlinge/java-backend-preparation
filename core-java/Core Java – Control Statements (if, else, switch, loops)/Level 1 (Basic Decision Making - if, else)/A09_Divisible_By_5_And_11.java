1a95f61e3d89aca2ce16245daafe12cc83febc39
PratikAdlinge <pratikadlinge2121@gmail.com>
2026-08-19 22:46:42 +0530

    some new logic

--- core-java/Core Java – Control Statements (if, else, switch, loops)/Level 1 (Basic Decision Making - if, else)/A09_Divisible_By_5_And_11.java
+++ core-java/Core Java – Control Statements (if, else, switch, loops)/Level 1 (Basic Decision Making - if, else)/A09_Divisible_By_5_And_11.java
@@ -6,18 +6,18 @@ public class A09_Divisible_By_5_And_11 {
         System.out.println("enter a no ");
         int no=sc.nextInt();
         if (no%5==0 && no%11==0) {
-            System.out.println("no is divisible by both 5 and 11");
+            System.out.println("number is divisible by both 5 and 11");
             
         }
         else if(no%5==0) {
-            System.out.println("no is divisible by 5 only");
+            System.out.println("number is divisible by 5 only");
 
         }
         else if(no%11==0) {
-            System.out.println("no is divisible by 11 only ");
+            System.out.println("number is divisible by 11 only ");
          }
          else {
-            System.out.println("no is not divisible by 5 and 11");
+            System.out.println("number is not divisible by 5 and 11");
          }
     }
 }
