463e2f50870e4d060b1c51a14c2871f8646f1c0f
PratikAdlinge <pratikadlinge2121@gmail.com>
2026-08-22 11:18:40 +0530

    Added P07 static method in interface example with bank amount validation

---
+++ OOPS/Interface_Programs/P07_BankServiceStaticMethod.java
@@ -0,0 +1,29 @@
+package OOPS.Interface_Programs;
+
+import java.util.Scanner;
+
+interface BankService {
+    void deposit(double amount);
+    static void validateAmount(double amount){
+        if (amount>0) {
+            System.out.println("Valid Amount.");
+        } else {
+            System.out.println("InValid Amount.");
+        }
+    }
+}
+class SavingAccount implements BankService {
+    public void deposit (double amount) {
+        System.out.println(amount);
+    }
+}
+public class P07_BankServiceStaticMethod {
+    public static void main(String[] args) {
+        SavingAccount s1=new SavingAccount();
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Amount for deposite:");
+        double amt=sc.nextDouble();
+        BankService.validateAmount(amt);
+        s1.deposit(amt);
+    }
+}
