5c7d4d1fb11b84a569257c59d258f2d1052d9a1b
PratikAdlinge <pratikadlinge2121@gmail.com>
2026-09-02 18:48:49 +0530

    Convert Linked list to Array using toArray()

---
+++ Collection_Framework/List/LinkedList/P03_Linkedlist_to_Array.java
@@ -0,0 +1,21 @@
+package Collection_Framework.List.LinkedList;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class P03_Linkedlist_to_Array {
+    public static void main(String[] args) {
+        List<String> names=new LinkedList<>();
+        names.add("Pratiksha");
+        names.add("Sejal");
+        names.add("Guddi");
+        names.add("Bandya");
+
+        System.out.println(names);
+
+       Object obj[]= names.toArray();
+       for (Object object : obj) {
+        System.out.println(object);
+       }
+    }
+}
