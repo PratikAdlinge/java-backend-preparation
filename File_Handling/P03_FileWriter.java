99e02e98f77099805976c594549e59660982b5b4
PratikAdlinge <pratikadlinge2121@gmail.com>
2026-09-04 16:21:19 +0530

    Daata Wrote in File

---
+++ File_Handling/P03_FileWriter.java
@@ -0,0 +1,18 @@
+package File_Handling;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class P03_FileWriter  {
+    public static void main(String[] args)throws IOException {
+        FileWriter f=new FileWriter("C:\\Users\\ADMIN\\OneDrive\\Desktop\\file1.txt");
+
+        f.write("Welcome To Simca ... tumch Kalyan Zal...");
+        f.close();
+        System.out.println("Successfuly data wrote in file");
+
+        PrintWriter.nullWriter();
+        
+    }
+}
