c0a2f81bce6eb203d9f54e1d2eba2b78ab974928
PratikAdlinge <pratikadlinge2121@gmail.com>
2026-09-06 14:42:06 +0530

    Implemented thread sleep using Thread.sleep() to pause thread execution for a specified time.

---
+++ MultiThreading/P04_SleepMethod.java
@@ -0,0 +1,39 @@
+package MultiThreading;
+
+class ThreadNames extends Thread {
+    public void run ()
+    {
+        try {
+            String n=Thread.currentThread().getName();
+
+        for (int i=0;i<3;i++){
+        System.out.println(n);
+        Thread.sleep(1000);
+    }
+        }
+         catch (InterruptedException e) {
+            // TODO: handle exception
+            System.out.println("Hnadled Exception:");
+        }
+        
+
+       
+}
+}
+public class P04_SleepMethod {
+    public static void main(String[] args) {
+        ThreadNames t1=new ThreadNames();
+        ThreadNames t2=new ThreadNames();
+        ThreadNames t3=new ThreadNames();
+
+        t1.setName("Thread 1");
+        t2.setName("Thread 2");
+        t3.setName("Thread 3");
+
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}
+
+
