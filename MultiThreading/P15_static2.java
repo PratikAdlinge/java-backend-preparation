0ceccf4c798316af214065a1f241535b0d39a218
PratikAdlinge <pratikadlinge2121@gmail.com>
2026-09-07 19:42:23 +0530

    Added another example

---
+++ MultiThreading/P15_static2.java
@@ -0,0 +1,37 @@
+package MultiThreading;
+
+class Bank extends  Thread {
+    static int balance=5000;
+    int withdraw;
+    Bank(int withdraw) {
+        this.withdraw=withdraw;
+    }
+    public static synchronized void withdraw(int amount) {
+        String name=Thread.currentThread().getName();
+        if(amount<=balance) {
+            System.out.println(name+" Withdraw Money...");
+            balance=balance-amount;
+        }
+        else {
+            System.out.println("Insufficient Funds..");
+        }
+    }
+    public void run () {
+        withdraw(this.withdraw);
+    }
+}
+    public class P15_static2 {
+    public static void main(String[] args) {
+        
+    
+    Bank obj=new Bank(10000);
+    Thread t1=new Thread(obj);
+    Thread t2=new Thread(obj);
+    t1.setName("Pratiksha");
+    t2.setName("Pratik");
+
+    t1.start();
+    t2.start();
+    }
+    
+}
