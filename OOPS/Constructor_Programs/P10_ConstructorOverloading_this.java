37d4b3f6784a062f3d684bcda0a5c4e9e41d2908
PratikAdlinge <pratikadlinge2121@gmail.com>
2026-09-10 23:01:02 +0530

    Implemented constructor overloading and constructor chaining using this() with multiple parameterized constructors.

---
+++ OOPS/Constructor_Programs/P10_ConstructorOverloading_this.java
@@ -0,0 +1,24 @@
+package OOPS.Constructor_Programs;
+class SIMCA{
+    SIMCA(){
+        System.out.println("Welcome to Simca");
+        this("Tumch kalyan zal");
+    }
+
+    SIMCA(String str){
+        System.out.println(str);
+        this(68000,"varyvar gel");
+    }
+
+    SIMCA(int fees,String msg){
+        System.out.println("Invested Money"+fees);
+        System.out.println(msg);
+
+    }
+}   
+
+public class P10_ConstructorOverloading_this {
+    public static void main(String[] args) {
+        SIMCA s1=new SIMCA();
+    }
+}
